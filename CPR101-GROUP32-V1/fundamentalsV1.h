06d6ce0051da8b98ac632455b3880b4250c04bed
Adnan094 <adnan2rocks@gmail.com>
2024-11-26 15:59:39 -0500

    Add files via upload

---
+++ CPR101-GROUP32-V1/fundamentalsV1.h
@@ -0,0 +1,12 @@
+#ifndef FUNDAMENTALS_H  // If FUNDAMENTALS_H is not defined
+#define FUNDAMENTALS_H  // Define FUNDAMENTALS_H
+
+// Include necessary standard libraries here:
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Function prototype declaration
+void fundamentals(void);
+
+#endif // End of the FUNDAMENTALS_H check
