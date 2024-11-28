482af1f82cbf0c06493e619d5173ed9730250029
amymengxy <amymengxy@gmail.com>
2024-11-27 21:42:28 -0500

    Add files via upload

--- CPR101-GROUP32-v2/manipulating-v2.c
+++ CPR101-GROUP32-v2/manipulating-v2.c
@@ -1,5 +1,6 @@
-#include "manipulating.h"
 #define CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE  
+#include "manipulating.h"
 #define BUFFER_SIZE 80
 
 
@@ -12,24 +13,62 @@ void manipulating()
 	do
 	{
 		printf("Type the 1st strig(q - to quit):\n"); //Prompt user to input string
-		//Get the string input by the user and store it in string1. The length of the string is less than or equal to BUFFER_SIZE.
+		//Get the string input by the user and store it in string1. The length of the string is less than BUFFER_SIZE.
 		fgets(string1, BUFFER_SIZE, stdin); 
-		// Replace the last character of the string1 with a null terminator. This marks the end of the string.
-		string1[strlen(string1) - 1] = '\0';
 
-		if ((strcmp(string1, "q") != 0)) // If string1 is not 'q', continue. Otherwise, the program ends after displaying an end prompt.
+		if (strchr(string1, '\n') == NULL)  //  If there is no newline character, it means the input has exceeded the buffer size.
 		{
-			printf("Type the 2nd string: \n"); // Prompt user to input string
-			//Get the string input by the user and store it in string2. The length of the string is less than or equal to BUFFER_SIZE
-			fgets(string2, BUFFER_SIZE, stdin);
-
-			// Replace the last character of the string2 with a null terminator. This marks the end of the string.
-			string2[strlen(string2) - 1] = '\0';   
-
-			strcat_s(string1, BUFFER_SIZE, string2);   // Sting2 connects to the end of string1.
-			//Display the string1 on the screen. at this time, the value of string1 is original string1 connected string2.
-			printf("Concatenated string is\'%s\'\n", string1); 
+			
+			while (getchar() != '\n')  //Read and discard characters from the input stream one by one until a newline character is encountered.
+			{
+				
+			}
+			printf("Input exceeded the string length limit(Maximum 78 characters), please try again..\n");
+			
 		}
+		else
+		{
+
+			// Replace the last character of the string1 with a null terminator. This marks the end of the string.
+			string1[strlen(string1) - 1] = '\0';
+
+			if ((strcmp(string1, "q") != 0)) // If string1 is not 'q', continue. Otherwise, the program ends after displaying an end prompt.
+			{
+				printf("Type the 2nd string: \n"); // Prompt user to input string
+				//Get the string input by the user and store it in string2. The length of the string is less than BUFFER_SIZE
+				fgets(string2, BUFFER_SIZE, stdin);
+				if (strchr(string2, '\n') == NULL)  //  If there is no newline character, it means the input has exceeded the buffer size.
+				{
+
+					while (getchar() != '\n')  //Read and discard characters from the input stream one by one until a newline character is encountered.
+					{
+
+					}
+					printf("Input exceeded the string length limit(Maximum 78 characters), please try again..\n");
+
+				}
+				else
+				{
+					// Replace the last character of the string2 with a null terminator. This marks the end of the string.
+					string2[strlen(string2) - 1] = '\0';
+					// If the total length of two strings is not exceeded the string length limit, Sting2 connects to the end of string1. ohterwise, prompt user the length exceeded.
+					if ((strlen(string1) + strlen(string2)) <= BUFFER_SIZE - 1) 
+					{
+						strcat(string1, string2);   // Sting2 connects to the end of string1.
+						//Display the string1 on the screen. at this time, the value of string1 is original string1 connected string2.
+						printf("Concatenated string is\'%s\'\n", string1);
+					}
+					else
+					{
+						printf("The total length of two strings exceeded the string length limit(Maximum 79 characters), please try again..\n");
+					}
+
+					
+				}
+				
+			}
+		}
+		
 	} while (strcmp(string1, "q") != 0); //If string1 is not "q", return to the beginning of the do-while loop and continue Otherwise, the program ends after displaying an end prompt.
 	printf("*** End of Concatenating ***\n"); 
 	
@@ -42,41 +81,69 @@ void manipulating()
 	do
 	{
 		printf("Type the 1st string to compare(q - to quit):\n");   //Prompt user to input string
-		//Get the string input by the user and store it in compare1. The length of the string is less than or equal to BUFFER_SIZE.
+		//Get the string input by the user and store it in compare1. The length of the string is less than BUFFER_SIZE.
 		fgets(compare1, BUFFER_SIZE, stdin);
-		// Replace the last character of the compare1 with a null terminator. This marks the end of the string.
-		compare1[strlen(compare1) - 1] = '\0';
 
-		if (strcmp(compare1, "q") != 0)  // If compare1 is not 'q', continue. Otherwise, the program ends after displaying an end prompt.
+		if (strchr(compare1, '\n') == NULL)  //  If there is no newline character, it means the input has exceeded the buffer size.
 		{
-			printf("Type the 2nd string to compare:\n");  // Prompt user to input string 
-			////Get the string input by the user and store it in compare2. The length of the string is less than or equal to BUFFER_SIZE.
-			fgets(compare2, BUFFER_SIZE, stdin);
-			// Replace the last character of the compare2 with a null terminator. This marks the end of the string.
-			compare2[strlen(compare2) - 1] = '\0';
 
-			result = strcmp(compare1, compare2); //Compare the string "compare1" and "compare2", store the result to int variable "result"
-			if (result < 0)
+			while (getchar() != '\n')  //Read and discard characters from the input stream one by one until a newline character is encountered.
 			{
-				// If result is less than 0, display compare1 is less than compare2
-				printf("\'%s\' string is less than \'%s\'\n", compare1, compare2);
+
 			}
-			else if (result == 0)
+			printf("Input exceeded the string length limit(Maximum 78 characters), please try again.\n");
+			
+		}
+		else
+		{
+			// Replace the last character of the compare1 with a null terminator. This marks the end of the string.
+			compare1[strlen(compare1) - 1] = '\0';
+
+			if (strcmp(compare1, "q") != 0)  // If compare1 is not 'q', continue. Otherwise, the program ends after displaying an end prompt.
 			{
-				// If result is equal to 0, display compare1 is equal to compare2
-				printf("\'%s\' string is equal to \'%s\'\n", compare1, compare2);
-			}
-			else
-			{
-				// If result is greater than 0, display compare1 is greater than compare2
-				printf("\'%s\' string is greater than \'%s\'\n", compare1, compare2);
+				printf("Type the 2nd string to compare:\n");  // Prompt user to input the 2nd string 
+				////Get the string input by the user and store it in compare2. The length of the string is less than BUFFER_SIZE.
+				fgets(compare2, BUFFER_SIZE, stdin);
+				if (strchr(compare2, '\n') == NULL)  //  If there is no newline character, it means the input has exceeded the buffer size.
+				{
+
+					while (getchar() != '\n')  //Read and discard characters from the input stream one by one until a newline character is encountered.
+					{
+
+					}
+					printf("Input exceeded the string length limit(Maximum 78 characters), please try again.\n");
+				}
+				else
+				{
+					// Replace the last character of the compare2 with a null terminator. This marks the end of the string.
+					compare2[strlen(compare2) - 1] = '\0';
+
+					result = strcmp(compare1, compare2); //Compare the string "compare1" and "compare2", store the result to int variable "result"
+					if (result < 0)
+					{
+						// If result is less than 0, display compare1 is less than compare2
+						printf("\'%s\' string is less than \'%s\'\n", compare1, compare2);
+					}
+					else if (result == 0)
+					{
+						// If result is equal to 0, display compare1 is equal to compare2
+						printf("\'%s\' string is equal to \'%s\'\n", compare1, compare2);
+					}
+					else
+					{
+						// If result is greater than 0, display compare1 is greater than compare2
+						printf("\'%s\' string is greater than \'%s\'\n", compare1, compare2);
+					}
+				}
+
+				
 			}
 		}
+			
 	} while (strcmp(compare1, "q") != 0); //If compare1 is not "q", return to the beginning of the do-while loop and continue. Otherwise, the program ends after displaying an end prompt.
 	printf("*** End of Comparing String Demo ***\n");
 
 /* Version 3 */
-//>> insert here
+	
 
-
-}
+} 
--- CPR101-GROUP32-v2/tokenizing-v2.c
+++ CPR101-GROUP32-v2/tokenizing-v2.c
@@ -1,40 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_DEPRECATE  
 #include "tokenizing.h"
 #define BUFFER_SIZE 300
-#define _CRT_SECURE_NO_WARNINGS
+
+
 
 void tokenizing()
 {
 /* Version 1 */
-	printf("*** Start of Tokenizing Words Demo ***\n");
+	 printf("*** Start of Tokenizing Words Demo ***\n");
 	char  words[BUFFER_SIZE];   // Declare a char array words with a size of 300.
 	char* nextWord = NULL;      // Initialize the char pointer nextWord to NULL.
-	int   wordsCounter;         // Declare a int varaible wordsCounter.
-	char* context = NULL;       // Initialize the char pointer context to NULL.
+	int   wordsCounter;         // Declare a int varaible wordsCounter used to store the number of tokenizing.
+	int  isQuit = 0;             // Declare a int varaible isQuit.isQuit = 1 means the user wants to exit the program.
 	do
 	{
 		printf("Type a few words separated by space (q - to quit):\n"); //Prompt user to input string
 
 		//Get the string input by the user and store it in words. The length of the string is less than or equal to BUFFER_SIZE.
 		fgets(words, BUFFER_SIZE, stdin);
-		// Replace the last character of the words with a null terminator. This marks the end of the string.
-		words[strlen(words) - 1] = '\0'; 
 
-		if (strcmp(words, "q") != 0)  //If words is not 'q', continue. Otherwise, the program ends after displaying an end prompt.
+		if (strchr(words, '\n') == NULL)  //  If there is no newline character, it means the input has exceeded the buffer size.
 		{
-			//Tokenize the string stored in 'words' using space as delimiter and store the first token in 'nextWord'. The 'context' keeps track of the current position for subsequent strtok_s calls.
-			nextWord = strtok_s(words, " ", &context);
 
-			wordsCounter = 1;  //wordsCounter used to store the number of tokenizing.
-
-			while (nextWord) // if nextWord is not null, enter the while loop.
+			while (getchar() != '\n')  //Read and discard characters from the input stream one by one until a newline character is encountered.
 			{
-				// Display the Sequence number of tokenized word and tokenized word; Then wordsCount plus 1. 
-				printf("Word #%d is \'%s\'\n", wordsCounter++, nextWord);
-				//Tokenize the string stored in 'words' using space as delimiter and store the first token in 'nextWord' again.
-				nextWord = strtok_s(NULL, " ", &context); 
+
+			}
+			printf("Input exceeded the string length limit(Maximum 298 characters), please try again..\n");
+
+		}
+		else
+		{
+			// Replace the last character of the words with a null terminator. This marks the end of the string.
+			words[strlen(words) - 1] = '\0';
+
+			if (strcmp(words, "q") != 0)  //If words is not 'q', continue. Otherwise, the program ends after displaying an end prompt.
+			{
+				//Tokenize the string stored in 'words' using space as delimiter and store the first token in 'nextWord'. 
+				nextWord = strtok(words, " ");
+
+				wordsCounter = 1;  //wordsCounter used to store the number of tokenizing.
+
+				while (nextWord) // if nextWord is not null, enter the while loop.
+				{
+					// Display the Sequence number of tokenized word and tokenized word; Then wordsCount plus 1. 
+					printf("Word #%d is \'%s\'\n", wordsCounter++, nextWord);
+					//Tokenize the string stored in 'words' using space as delimiter and store the first token in 'nextWord' again.
+					nextWord = strtok(NULL, " ");
+				}
+			}
+			else
+			{
+				isQuit = 1;
 			}
 		}
-	} while (strcmp(words, "q") != 0); ////If words is not "q", return to the beginning of the do-while loop and continue. Otherwise, the program ends after displaying an end prompt.
+		
+	} while (!isQuit); // isQuit = 0 means return to the beginning of the do-while loop and continue. Otherwise, the program ends after displaying an end prompt.
 	printf("*** End of Tokenizing Words Demo ***\n\n");
 	
 
@@ -42,36 +64,52 @@ void tokenizing()
 	printf("*** Start of Tokenizing Phrases Demo ***\n");
 	char   phrases[BUFFER_SIZE];  //Declare a char array phrase with a size of 300.
 	char*  nextPhrase = NULL;     // Initialize the char pointer nextPhrase to NULL.
-	int    phrasesCounter;        // Declare a int varaible phrasesCounter used to store the number of the tokenized word 
-	context = NULL;               // assign the char pointer context to NULL.
+	int    phrasesCounter;        // Declare a int varaible phrasesCounter used to store the number of the tokenized phrases. 
+	isQuit = 0;                   // reset isQuit.
 	
 	do
 	{
 		printf("Type a few phrases separated by comma(q - to quit):\n");  //Prompt user to input a few phrases
 		//Get the string input by the user and store it in phrases. The length of the string is less than or equal to BUFFER_SIZE.
 		fgets(phrases, BUFFER_SIZE, stdin);
-		// Replace the last character of the phrases with a null terminator. This marks the end of the string.
-		phrases[strlen(phrases) - 1] = '\0';
-		if (strcmp(phrases, "q") != 0) //If phrases is not 'q', continue.Otherwise, the program ends after displaying an end prompt.
+		if (strchr(phrases, '\n') == NULL)  //  If there is no newline character, it means the input has exceeded the buffer size.
 		{
-			//Tokenize the string stored in "phrases" using comma as delimiter and store the first token in 'nextPhrase'. The 'context' keeps track of the current position for subsequent strtok_s calls.
-			nextPhrase = strtok_s(phrases, ",", &context);
-			
-			phrasesCounter = 1;  //phrasesCounter used to store the number of tokenizing.
-			while (nextPhrase)   //if nextPhrase is not null, enter the while loop.
+
+			while (getchar() != '\n')  //Read and discard characters from the input stream one by one until a newline character is encountered.
 			{
-				// Display the Sequence number of tokenized phrase and tokenized phrase; Then phrasesCount plus 1. 
-				printf("Phrase #%d is \'%s\'\n", phrasesCounter++, nextPhrase);
-				//Tokenize the string stored in "phrases" using comma as delimiter and store the first token in 'nextPhrase' again.
-				nextPhrase = strtok_s(NULL, ",", &context);				
+
+			}
+			printf("Input exceeded the string length limit(Maximum 298 characters), please try again..\n");
+
+		}
+		else
+		{
+			// Replace the last character of the phrases with a null terminator. This marks the end of the string.
+			phrases[strlen(phrases) - 1] = '\0';
+			if (strcmp(phrases, "q") != 0) //If phrases is not 'q', continue.Otherwise, the program ends after displaying an end prompt.
+			{
+				//Tokenize the string stored in "phrases" using comma as delimiter and store the first token in 'nextPhrase'. 
+				nextPhrase = strtok(phrases, ",");
+
+				phrasesCounter = 1;  //phrasesCounter used to store the number of tokenizing.
+				while (nextPhrase)   //if nextPhrase is not null, enter the while loop.
+				{
+					// Display the Sequence number of tokenized phrase and tokenized phrase; Then phrasesCount plus 1. 
+					printf("Phrase #%d is \'%s\'\n", phrasesCounter++, nextPhrase);
+					//Tokenize the string stored in "phrases" using comma as delimiter and store the first token in 'nextPhrase' again.
+					nextPhrase = strtok(NULL, ",");
+				}
+			}
+			else
+			{
+				isQuit = 1;
 			}
 		}
-	} while (strcmp(phrases, "q") != 0);  //If phrases is not "q", return to the beginning of the do-while loop and continue. Otherwise, the program ends after displaying an end prompt.
+		
+	} while (!isQuit);  //isQuit = 0 means return to the beginning of the do-while loop and continue. Otherwise, the program ends after displaying an end prompt.
 	printf("*** End of Tokenizing Phrases Demo ***\n\n");
 
 
 /* Version 3 */
-//>> insert here
-
-
+	
 }
