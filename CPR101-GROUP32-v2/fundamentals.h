3b15da00c1454eceea8ca98f9d0d9f79c0169e85
Adnan094 <adnan2rocks@gmail.com>
2024-11-30 11:37:07 -0500

    Add files via upload

---
+++ CPR101-GROUP32-v2/fundamentals.h
@@ -0,0 +1,13 @@
+// FUNDAMENTALS MODULE HEADER
+#ifndef _FUNDAMENTALS_H_
+#define _FUNDAMENTALS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Function declaration
+void fundamentals(void);
+
+#endif
+#pragma once
